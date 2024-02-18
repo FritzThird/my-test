b3f616a9b5ff749e9d6cbece72b40b26ed249417
FritzThird <35336464+FritzThird@users.noreply.github.com>
2024-02-18 00:51:17 -0500

    Update script_2.js

--- script_2.js
+++
@@ -1,6 +0,0 @@
-const hamburgerButton = document.getElementById('hamburger-button');
-const navLinks = document.getElementById('nav-links');
-
-hamburgerButton.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
